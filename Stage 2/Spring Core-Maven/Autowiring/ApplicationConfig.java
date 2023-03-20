574dcf26322e21edc1d18c7ad6217a916dbfe73c
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-03-20 13:16:11 +0530

    Corrected the code for Autowiring

--- Stage 2/Spring Core-Maven/Autowiring/ApplicationConfig.java
+++ Stage 2/Spring Core-Maven/Autowiring/ApplicationConfig.java
@@ -10,50 +10,32 @@ import org.springframework.context.annotation.Bean;
 @Configuration
 @ComponentScan
 public class ApplicationConfig {
-
-	@Bean
-	public Employee employee() {
-		// Creating an 'Employee' bean
-		Employee employee = new Employee();
-
-		// Setter-based injection:-
-
-		// Similar to <property name="empId" value="1234" />
-		employee.setEmpId(1234);
-
-		// Similar to <property name="empName" value="Abhishek" />
-		employee.setEmpName("Abhishek");
-
-		// The below statement isn't required due to 'autowiring'
-		// employee.setPassObj(passObj());
-
-		return employee;
-	}
-
-	@Bean
-	public Passport passObj() {
-
-		// Creating a 'Passport' bean
-		Passport passport = new Passport();
-
-		Date todayDate = new Date();
-
-		// Using 'Calendar' class to add 84 days to todayDate (expiryDate)
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(todayDate);
-		cal.add(Calendar.DATE, 84);
-
-		Date expiryDate = cal.getTime();
-
-		// Similar to <property name="passNum" value="9876" />
-		passport.setPassNum(9876);
-
-		// The below 2 setters cannot be used in an xml config file
-		// as they are dynamic values
-		passport.setDateOfIssue(todayDate);
-		passport.setDateOfExpiry(expiryDate);
-
-		return passport;
-	}
-
+    
+    @Bean
+    public Employee employee() {
+        Employee employee = new Employee();
+        employee.setEmpId(1234);
+        employee.setEmpName("Arijit");
+        
+        return employee;
+    }
+    
+    @Bean
+    public Passport passObj() {
+        Passport pp = new Passport();
+        
+        Date todayDate = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(todayDate);
+        cal.add(Calendar.DATE, 84);
+        
+        Date expiryDate = cal.getTime();
+        pp.setPassNum(2345);
+        
+        pp.setDateOfIssue(todayDate);
+        pp.setDateOfExpiry(expiryDate);
+        
+        return pp;
+    }
+	
 }
