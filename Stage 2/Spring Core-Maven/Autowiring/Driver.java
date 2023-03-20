6a1f361b46e3e449c6e0f99927ad487fd0c40c1c
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-03-20 13:10:28 +0530

    Revert "Corrected Autowiring"
    This reverts commit 55eb44b81e5588b2bac73e31be12ec0643a72756.

--- Stage 2/Spring Core-Maven/Autowiring/Driver.java
+++ Stage 2/Spring Core-Maven/Autowiring/Driver.java
@@ -4,15 +4,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Driver {
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        Employee employee = (Employee)context.getBean(Employee.class);
-        
-        System.out.println("EmpID : " + employee.getEmpId());
-        System.out.println("EmpName : " + employee.getEmpName());
-        System.out.println(employee.getPassObj().toString());
-        context.close();
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-    }
-	
+		Employee employee = (Employee) context.getBean(Employee.class);
+
+		System.out.println("EmpID : " + employee.getEmpId());
+		System.out.println("EmpName : " + employee.getEmpName());
+		System.out.println(employee.getPassObj().toString());
+		
+		context.close();
+	}
+
 }
