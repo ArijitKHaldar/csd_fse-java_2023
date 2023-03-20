55eb44b81e5588b2bac73e31be12ec0643a72756
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-03-20 12:48:48 +0530

    Corrected Autowiring

--- Stage 2/Spring Core-Maven/Autowiring/ApplicationConfig.java
+++ Stage 2/Spring Core-Maven/Autowiring/ApplicationConfig.java
@@ -11,49 +11,49 @@ import org.springframework.context.annotation.Bean;
 @ComponentScan
 public class ApplicationConfig {
 
-	@Bean
-	public Employee employee() {
-		// Creating an 'Employee' bean
-		Employee employee = new Employee();
+    @Bean
+    public Employee employee() {
+        // Creating an 'Employee' bean
+        Employee employee = new Employee();
 
-		// Setter-based injection:-
+        // Setter-based injection:-
 
-		// Similar to <property name="empId" value="1234" />
-		employee.setEmpId(1234);
+        // Similar to <property name="empId" value="1234" />
+        employee.setEmpId(1234);
 
-		// Similar to <property name="empName" value="Abhishek" />
-		employee.setEmpName("Abhishek");
+        // Similar to <property name="empName" value="Abhishek" />
+        employee.setEmpName("Abhishek");
 
-		// The below statement isn't required due to 'autowiring'
-		// employee.setPassObj(passObj());
+        // The below statement isn't required due to 'autowiring'
+        // employee.setPassObj(passObj());
 
-		return employee;
-	}
+        return employee;
+    }
 
-	@Bean
-	public Passport passObj() {
+    @Bean
+    public Passport passObj() {
 
-		// Creating a 'Passport' bean
-		Passport passport = new Passport();
+        // Creating a 'Passport' bean
+        Passport passport = new Passport();
 
-		Date todayDate = new Date();
+        Date todayDate = new Date();
 
-		// Using 'Calendar' class to add 84 days to todayDate (expiryDate)
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(todayDate);
-		cal.add(Calendar.DATE, 84);
+        // Using 'Calendar' class to add 84 days to todayDate (expiryDate)
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(todayDate);
+        cal.add(Calendar.DATE, 84);
 
-		Date expiryDate = cal.getTime();
+        Date expiryDate = cal.getTime();
 
-		// Similar to <property name="passNum" value="9876" />
-		passport.setPassNum(9876);
+        // Similar to <property name="passNum" value="9876" />
+        passport.setPassNum(9876);
 
-		// The below 2 setters cannot be used in an xml config file
-		// as they are dynamic values
-		passport.setDateOfIssue(todayDate);
-		passport.setDateOfExpiry(expiryDate);
+        // The below 2 setters cannot be used in an xml config file
+        // as they are dynamic values
+        passport.setDateOfIssue(todayDate);
+        passport.setDateOfExpiry(expiryDate);
 
-		return passport;
-	}
+        return passport;
+    }
 
 }
--- Stage 2/Spring Core-Maven/Autowiring/Driver.java
+++ Stage 2/Spring Core-Maven/Autowiring/Driver.java
@@ -4,16 +4,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Driver {
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        Employee employee = (Employee)context.getBean(Employee.class);
+        
+        System.out.println("EmpID : " + employee.getEmpId());
+        System.out.println("EmpName : " + employee.getEmpName());
+        System.out.println(employee.getPassObj().toString());
+        context.close();
 
-		Employee employee = (Employee) context.getBean(Employee.class);
-
-		System.out.println("EmpID : " + employee.getEmpId());
-		System.out.println("EmpName : " + employee.getEmpName());
-		System.out.println(employee.getPassObj().toString());
-		
-		context.close();
-	}
-
+    }
+	
 }
