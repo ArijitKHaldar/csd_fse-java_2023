5867ebbc8e8c1e0de7c124ecc375fa816f54ff69
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-05-16 22:32:54 +0530

    Removed fa-icon for popup close

--- IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/HomePage/PopupBox.js
+++ IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/HomePage/PopupBox.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { FaTimes } from "react-icons/fa";
+import React, { useEffect, useRef } from "react";
 import "./PopupBox.css";
 
 const PopupBox = ({ content, onClose }) => {
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [onClose]);
+
   return (
     <div className="popup-box">
-      <div className="popup-content">
-        <button className="close-icon" onClick={onClose}>
-          <FaTimes className="close-icon-cross" />
-        </button>
+      <div className="popup-content" ref={popupRef}>
         <p>{content}</p>
       </div>
     </div>
