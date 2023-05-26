44d01a63e23f2fb647c52381aacfe04c420c940a
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-05-26 12:46:43 +0530

    One business case implemented

--- IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/MonthlySavingsPercentage.js
+++ IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/MonthlySavingsPercentage.js
@@ -1,5 +1,107 @@
-function MonthlySavingsPercentage() {
-  return <></>;
+import React, { useEffect, useState } from "react";
+import { Progress } from "reactstrap";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import axios from "axios";
+import "./MonthlySavingsPercentage.css";
+
+function MonthlySavingsPercentage({ userId }) {
+  const [savingsPercentage, setSavingsPercentage] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+
+  useEffect(() => {
+    axios
+      .get(
+        `/api/savings/calculation/v1/user/${userId}/savings/month/${selectedMonth}`
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          setSavingsPercentage(response.data);
+        } else {
+          setSavingsPercentage(0);
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          setSavingsPercentage(0);
+        } else {
+          console.log("Error fetching monthly savings percentage:", error);
+        }
+      });
+  }, [userId, selectedMonth]);
+
+  const handleMonthChange = (e) => {
+    setSelectedMonth(Number(e.target.value));
+  };
+
+  const COLORS = ["#0088FE", "#00C49F"];
+
+  const pieChartData = [
+    {
+      id: "savings",
+      name: `Savings ${savingsPercentage}%`,
+      value: savingsPercentage,
+    },
+    {
+      id: "expenditure",
+      name: `Expenditure ${100 - savingsPercentage}%`,
+      value: 100 - savingsPercentage,
+    },
+  ];
+
+  return (
+    <div className="table-section">
+      <div className="monthly-savings-percentage child-component income-section">
+        <h2>Monthly Savings Percentage</h2>
+        <div className="month-dropdown">
+          <label htmlFor="month">Select Month: </label>
+          <select id="month" value={selectedMonth} onChange={handleMonthChange}>
+            <option value={1}>January</option>
+            <option value={2}>February</option>
+            <option value={3}>March</option>
+            <option value={4}>April</option>
+            <option value={5}>May</option>
+            <option value={6}>June</option>
+            <option value={7}>July</option>
+            <option value={8}>August</option>
+            <option value={9}>September</option>
+            <option value={10}>October</option>
+            <option value={11}>November</option>
+            <option value={12}>December</option>
+          </select>
+        </div>
+        <div className="progress-bar">
+          <Progress
+            value={savingsPercentage}
+          >{`${savingsPercentage}%`}</Progress>
+        </div>
+        <div className="pie-chart">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={pieChartData}
+              cx="50%"
+              cy="50%"
+              innerRadius={100}
+              outerRadius={170}
+              fill="#8884d8"
+              dataKey="value"
+              startAngle={90}
+              endAngle={-270}
+              paddingAngle={1}
+            >
+              {pieChartData.map((entry) => (
+                <Cell
+                  key={entry.id}
+                  fill={COLORS[pieChartData.indexOf(entry) % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend />
+            <Tooltip />
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MonthlySavingsPercentage;
--- IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/YearlySavingsPercentage.js
+++ IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/YearlySavingsPercentage.js
@@ -1,5 +1,109 @@
-function YearlySavingsPercentage() {
-  return <></>;
+import React, { useEffect, useState } from "react";
+import { Progress } from "reactstrap";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import axios from "axios";
+
+function YearlySavingsPercentage({ userId }) {
+  const [savingsPercentage, setSavingsPercentage] = useState(0);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    axios
+      .get(
+        `/api/savings/calculation/v1/user/${userId}/savings/year/${selectedYear}`
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          setSavingsPercentage(response.data);
+        } else {
+          setSavingsPercentage(0);
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          setSavingsPercentage(0);
+        } else {
+          console.log("Error fetching yearly savings percentage:", error);
+        }
+      });
+  }, [userId, selectedYear]);
+
+  const handleYearChange = (e) => {
+    setSelectedYear(Number(e.target.value));
+  };
+
+  const COLORS = ["#0088FE", "#00C49F"];
+
+  const pieChartData = [
+    {
+      id: "savings",
+      name: `Savings ${savingsPercentage}%`,
+      value: savingsPercentage,
+    },
+    {
+      id: "expenditure",
+      name: `Expenditure ${100 - savingsPercentage}%`,
+      value: 100 - savingsPercentage,
+    },
+  ];
+
+  return (
+    <div className="table-section">
+      <div className="yearly-savings-percentage child-component income-section">
+        <h2>Yearly Savings Percentage</h2>
+        <div className="yearly-dropdown">
+          <label htmlFor="year">Select Year: </label>
+          <select id="year" value={selectedYear} onChange={handleYearChange}>
+            <option value={new Date().getFullYear() - 2}>
+              {new Date().getFullYear() - 2}
+            </option>
+            <option value={new Date().getFullYear() - 1}>
+              {new Date().getFullYear() - 1}
+            </option>
+            <option value={new Date().getFullYear()}>
+              {new Date().getFullYear()}
+            </option>
+            <option value={new Date().getFullYear() + 1}>
+              {new Date().getFullYear() + 1}
+            </option>
+            <option value={new Date().getFullYear() + 2}>
+              {new Date().getFullYear() + 2}
+            </option>
+          </select>
+        </div>
+        <div className="progress-bar">
+          <Progress
+            value={savingsPercentage}
+          >{`${savingsPercentage}%`}</Progress>
+        </div>
+        <div className="pie-chart">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={pieChartData}
+              cx="50%"
+              cy="50%"
+              innerRadius={100}
+              outerRadius={170}
+              fill="#8884d8"
+              dataKey="value"
+              startAngle={90}
+              endAngle={-270}
+              paddingAngle={1}
+            >
+              {pieChartData.map((entry) => (
+                <Cell
+                  key={entry.id}
+                  fill={COLORS[pieChartData.indexOf(entry) % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend />
+            <Tooltip />
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default YearlySavingsPercentage;
