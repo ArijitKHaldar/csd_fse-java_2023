1fdd44bb1a76703877bbab83a4bbfc6e10f4af79
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-05-27 13:29:38 +0530

    Chart to show savings

--- IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/PredictSavings.js
+++ IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/PredictSavings.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 function PredictSavings({ userId }) {
   const [predictedSavings, setPredictedSavings] = useState(null);
+  const [monthlyData, setMonthlyData] = useState([]);
 
   useEffect(() => {
     const fetchPredictedSavings = async () => {
@@ -15,10 +24,68 @@ function PredictSavings({ userId }) {
       }
     };
 
+    const fetchMonthlyData = async () => {
+      try {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const monthDataPromises = [];
+        for (let i = 1; i <= 12; i++) {
+          monthDataPromises.push(fetchMonthData(userId, currentYear, i));
+        }
+        const monthlyData = await Promise.all(monthDataPromises);
+        setMonthlyData(monthlyData);
+      } catch (error) {
+        console.log("Error fetching monthly data:", error);
+      }
+    };
+
     fetchPredictedSavings();
+    fetchMonthlyData();
   }, [userId]);
 
+  const fetchMonthData = async (userId, year, month) => {
+    try {
+      const incomeResponse = await axios.get(
+        `/api/income/v1/userid/${userId}/month/${month}`
+      );
+      const expenditureResponse = await axios.get(
+        `/api/expenditure/v1/userid/${userId}/month/${month}`
+      );
+      const incomeAmount = incomeResponse.data.reduce(
+        (total, income) => total + income.incomeAmount,
+        0
+      );
+      const expenditureAmount = expenditureResponse.data.reduce(
+        (total, expenditure) => total + expenditure.expenditureAmount,
+        0
+      );
+      const savings = incomeAmount - expenditureAmount;
+      return { month: month.toString(), savings };
+    } catch (error) {
+      console.log(`Error fetching data for month ${month}:`, error);
+      return { month: month.toString(), savings: 0 };
+    }
+  };
+
   const getMonthString = (monthIndex) => {
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return monthNames[monthIndex - 1];
+  };
+
+  const getFullMonthString = (monthIndex) => {
     const monthNames = [
       "January",
       "February",
@@ -37,11 +104,12 @@ function PredictSavings({ userId }) {
   };
 
   const currentMonth = getMonthString(new Date().getMonth() + 2);
+  const currentFullMonth = getFullMonthString(new Date().getMonth() + 2);
 
   return (
     <div className="table-section">
       <div className="predict child-component income-section">
-        <h2>Predicted Savings of {currentMonth}</h2>
+        <h2>Predicted Savings of {currentFullMonth}</h2>
         {predictedSavings !== null ? (
           <p>
             Based on the financial data you have entered, the predicted savings
@@ -49,6 +117,35 @@ function PredictSavings({ userId }) {
           </p>
         ) : (
           <p>Could not predict savings</p>
+        )}
+        {monthlyData.length > 0 && (
+          <div className="chart-container">
+            <AreaChart
+              width={1000}
+              height={300}
+              data={monthlyData}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 10,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="month"
+                tickFormatter={(value) => getMonthString(parseInt(value))}
+              />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="savings"
+                fill="#8884d8"
+                stroke="#8884d8"
+              />
+            </AreaChart>
+          </div>
         )}
       </div>
     </div>
