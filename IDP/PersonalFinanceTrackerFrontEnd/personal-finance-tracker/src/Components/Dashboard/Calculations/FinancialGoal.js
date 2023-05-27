afc7bc4b73e77d6483acb7260967962c8478546d
Arijit Kumar Haldar <8902064-ArijitKHaldar@users.noreply.gitlab.com>
2023-05-27 13:26:48 +0530

    Made chart for maximum expenditure category

--- IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/FinancialGoal.js
+++ IDP/PersonalFinanceTrackerFrontEnd/personal-finance-tracker/src/Components/Dashboard/Calculations/FinancialGoal.js
@@ -1,12 +1,100 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form, FormGroup, Input, Label, Progress } from "reactstrap";
 import axios from "axios";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function FinancialGoal({ userId }) {
   const [date, setDate] = useState("");
   const [amount, setAmount] = useState("");
   const [percentageCompletion, setPercentageCompletion] = useState(null);
   const [error, setError] = useState(null);
+  const [expenditureData, setExpenditureData] = useState([]);
+  const [categoryData, setCategoryData] = useState([]);
+  const [occurrencesData, setOccurrencesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/api/expenditure/v1/userid/${userId}`)
+      .then((response) => {
+        setExpenditureData(response.data);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          setExpenditureData([]);
+        } else {
+          console.log("Error fetching expenditure data:", error);
+        }
+      });
+
+    axios
+      .get(`/api/category/v1/view`)
+      .then((response) => {
+        setCategoryData(response.data);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          setCategoryData([]);
+        } else {
+          console.log("Error fetching income data:", error);
+        }
+      });
+  }, [userId]);
+
+  useEffect(() => {
+    const getOccurrencesByMonthAndCategory = () => {
+      const occurrencesByMonthAndCategory = {};
+
+      expenditureData.forEach((expenditure) => {
+        const month = new Date(expenditure.expenditureDate).toLocaleString(
+          "en-US",
+          { month: "short" }
+        );
+        const categoryId = expenditure.categoryId;
+
+        if (!occurrencesByMonthAndCategory[month]) {
+          occurrencesByMonthAndCategory[month] = {};
+        }
+
+        if (!occurrencesByMonthAndCategory[month][categoryId]) {
+          occurrencesByMonthAndCategory[month][categoryId] = {
+            occurrences: 0,
+            categoryId: categoryId,
+          };
+        }
+
+        occurrencesByMonthAndCategory[month][categoryId].occurrences++;
+      });
+
+      return Object.keys(occurrencesByMonthAndCategory).map((month) => {
+        const categoryOccurrences = occurrencesByMonthAndCategory[month];
+        const maxOccurrences = Math.max(
+          ...Object.values(categoryOccurrences).map(
+            (category) => category.occurrences
+          )
+        );
+        const categoryId = Object.keys(categoryOccurrences).find(
+          (categoryId) =>
+            categoryOccurrences[categoryId].occurrences === maxOccurrences
+        );
+        return {
+          month,
+          "Frequent Expenditure Category": maxOccurrences,
+          categoryId,
+        };
+      });
+    };
+
+    const occurrencesData = getOccurrencesByMonthAndCategory();
+    setOccurrencesData(occurrencesData);
+  }, [categoryData, expenditureData]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,6 +118,31 @@ function FinancialGoal({ userId }) {
       setError("Data Inconsistencies Found. Cannot calculate");
       console.log("Error calculating savings completion:", error);
     }
+  };
+
+  const getCategoryTag = (categoryId) => {
+    if (categoryData.length === 0) {
+      return "Loading...";
+    }
+    const category = categoryData.find(
+      (category) => category.categoryId === categoryId
+    );
+    return category ? category.expenditureTag : "No Data";
+  };
+
+  const customTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      const categoryId = payload[0].payload.categoryId;
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`Category: ${getCategoryTag(
+            parseInt(categoryId)
+          )}`}</p>
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -73,6 +186,22 @@ function FinancialGoal({ userId }) {
             </div>
           )
         )}
+
+        <div className="bar-chart">
+          <BarChart
+            width={600}
+            height={250}
+            data={occurrencesData}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip content={customTooltip} />
+            <Legend />
+            <Bar dataKey="Frequent Expenditure Category" fill="#8884d8" />
+          </BarChart>
+        </div>
       </div>
     </div>
   );
